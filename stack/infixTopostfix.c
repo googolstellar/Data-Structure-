33c9f7594ec9297ee9d1f4da9590e112ce8e364b
googolstellar <57088144+googolstellar@users.noreply.github.com>
2020-02-08 08:23:38 +0530

    Update infixTopostfix.c

--- stack/infixTopostfix.c
+++ stack/infixTopostfix.c
@@ -2,13 +2,14 @@
 #define MAX 100
 #define ERROR '$'
 char stack[MAX];
-int top = -1;
+int top = 0;
+
 typedef struct priority{
     int order;
     char oper;
 }priority;
 
-priority pri[] = { {0, '-'}, {0, '+'}, {2, '*'}, {2, '/'}, {3, '('} };
+priority pri[] = { {0, '-'}, {0, '+'}, {1, '*'}, {1, '/'}, {3, '('} };
 
 void push(char oper){
     if(top >= MAX - 1){
@@ -22,29 +23,24 @@ void push(char oper){
 }
 
 char pop(){
-    if(stack[top] != '$'){
+    if(stack[top] != '$' && top > 0){
         return stack[top--];
     }
     else{
-        printf("Stack is Empty");
         return ERROR;
     }
 }
 
 int priorityCheck(char oper){
     int i = 0, operPri, stackPri;
-    if(stack[top] != '$'){
-        while(i <= 4){
+    if(stack[top] != '$' && stack[top] != '('){
+        while(i < 5){
             if(oper == pri[i].oper)
                 operPri = pri[i].order;
-            else if(stack[top] == pri[i].oper)
+            if(stack[top] == pri[i].oper)
                 stackPri = pri[i].order;
             i++;
         }
-        // if(i > 4){
-        //     printf("Invalid operator!");
-        //     return ERROR;
-        // }
         if(operPri > stackPri){
             return 1; //need to be pushed
         }
@@ -52,20 +48,23 @@ int priorityCheck(char oper){
             return 0; //need to be poped
         }
     }
-    else{
+    else if(stack[top] == '(' || stack[top] == '$'){
         return 1;
+    }
+    else{
+        return ERROR;
     }
 }
 
 void infixToPostfix(char infix[MAX], char postfix[MAX]){
     int i = 0, j = 0;
     char popOper;
-    while(infix[i++] != '\0'){
+    while(infix[i] != '\0' && infix[i] != '\n'){
         if(infix[i] >= '0' && infix[i] <= '9'){
-            postfix[j++] = infix[i];
+            postfix[j++] = infix[i++];
         }
         else{
-            if(infix[i++] == ')'){
+            if(infix[i] == ')'){
                 do{
                     popOper = pop();
                     if(popOper == ERROR)
@@ -75,23 +74,31 @@ void infixToPostfix(char infix[MAX], char postfix[MAX]){
                     }
                 }
                 while(popOper != '(');
-            }
-            else if(priorityCheck(infix[i])){
-                push(infix[i]);
+                i++;
             }
             else{
-                popOper = pop();
-                if(popOper != ERROR){
-                    postfix[j++] = popOper;
-                    push(infix[i]);
-                } 
-                else
-                    return;  
-            }
+                if(priorityCheck(infix[i]) == ERROR)
+                    return;
+                if(priorityCheck(infix[i]) == 1){
+                    push(infix[i++]);
+                }
+                else if(priorityCheck(infix[i]) == 0){
+                    popOper = pop();
+                    if(popOper != ERROR){
+                        postfix[j++] = popOper;
+                        push(infix[i++]);
+                    } 
+                    else
+                        return;  
+                }
+            } 
         }
     }
+    while(stack[top] != '$'){
+        popOper = pop();
+        postfix[j++] = popOper;
+    }
     postfix[j] = '\0';
-    printf("**%d ", i);
 }
 
 int main(){
@@ -101,10 +108,6 @@ int main(){
     printf("Enter a valid infix expression : ");
     fgets(infix, MAX, stdin);
     infixToPostfix(infix, postfix);
-    // while (postfix[i] != '\0')
-    // {
-    //     printf("%c", postfix[i++]);
-    // }
-    printf("$%s$\n", postfix);
-    
+    printf("Postfix notation for given infix notation is: ");
+    printf("%s\n", postfix);
 }
